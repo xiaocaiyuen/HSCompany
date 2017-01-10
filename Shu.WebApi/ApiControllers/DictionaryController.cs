ce33eb87240d76bc41e4f650995c8ae25f2a0e6d
xiaoliang <itxiaoliangseo@qq.com>
2017-01-10 14:37:08 +0800

    去掉日志备忘录

--- Shu.WebApi/ApiControllers/DictionaryController.cs
+++ Shu.WebApi/ApiControllers/DictionaryController.cs
@@ -1,22 +1,17 @@
-﻿using Shu.Utility.WebApi;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using Shu.Model;
-using Shu.WebApi.Models;
-using Shu.BLL;
-using System.Web.Http.Controllers;
 using Shu.Utility.Extensions;
 using Shu.Comm;
+using Shu.Model;
+using Shu.BLL;
+using Shu.Utility.WebApi;
 
 namespace Shu.WebApi.ApiControllers
 {
-    /// <summary>
-    /// 数据字典功能
-    /// </summary>
     public class DictionaryController : BaseController
     {
         /// <summary>
@@ -25,16 +20,16 @@ namespace Shu.WebApi.ApiControllers
         /// <param name="ParentCode">编号</param>
         /// <returns>数据实体</returns>
         [ActionName("DictionaryList")]
-        public WebApiResult<List<Sys_DataDict>> GetDictionary(string ParentCode)
+        public List<Sys_DataDict> GetDictionaryList(string ParentCode)
         {
             List<Sys_DataDict> DataDictList = new Sys_DataDictBLL().GetList(p => p.DataDict_ParentCode == ParentCode && p.DataDict_IsDel == false).OrderBy(p => p.DataDict_Sequence).ToList();
             DataDictList.Insert(0, new Sys_DataDict { DataDict_Name = Constant.DrpChoiceName, DataDict_Code = "" });
-            var result = new WebApiPagingResult<List<Sys_DataDict>>
-            {
-                Data = DataDictList
-            };
+            //var result = new WebApiPagingResult<List<Sys_DataDict>>
+            //{
+            //    Data = DataDictList
+            //};
 
-            return result;
+            return DataDictList;
         }
 
         /// <summary>
@@ -43,13 +38,13 @@ namespace Shu.WebApi.ApiControllers
         /// <param name="ParentCode">编号</param>
         /// <returns>数据实体</returns>
         [ActionName("DictionaryTree")]
-        public WebApiResult<List<combotree>> GetDictionaryTree(string ParentCode)
+        public List<combotree> GetDictionaryTree(string TreeCode)
         {
             List<combotree> tree = new List<combotree>() {
                 new combotree { id="",text=Constant.DrpChoiceName}
             };
-            List<Sys_DataDict> DataDictList = new Sys_DataDictBLL().GetList(p => p.DataDict_ParentCode.StartsWith(ParentCode) && p.DataDict_IsDel == false).OrderBy(p => p.DataDict_Sequence).ToList();
-            List<Sys_DataDict> OneDataDictList = DataDictList.Where(p => p.DataDict_ParentCode == ParentCode).ToList();
+            List<Sys_DataDict> DataDictList = new Sys_DataDictBLL().GetList(p => p.DataDict_ParentCode.StartsWith(TreeCode) && p.DataDict_IsDel == false).OrderBy(p => p.DataDict_Sequence).ToList();
+            List<Sys_DataDict> OneDataDictList = DataDictList.Where(p => p.DataDict_ParentCode == TreeCode).ToList();
 
             OneDataDictList.ForEach(item =>
             {
@@ -62,12 +57,12 @@ namespace Shu.WebApi.ApiControllers
                 });
             });
 
-            var result = new WebApiPagingResult<List<combotree>>
-            {
-                Data = tree
-            };
+            //var result = new WebApiPagingResult<List<combotree>>
+            //{
+            //    Data = tree
+            //};
 
-            return result;
+            return tree;
         }
 
         private List<combotree> children(string code, List<Sys_DataDict> treeList)
@@ -85,6 +80,5 @@ namespace Shu.WebApi.ApiControllers
             });
             return tree;
         }
-
     }
 }
